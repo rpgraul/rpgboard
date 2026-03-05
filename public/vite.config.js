4266dfe9f6faec7dc7f4d368253494f5736d3a4e
rpgraul <rpgraul@hotmail.com>
2026-03-05 18:34:27 -0300

    vite 3.0

--- public/vite.config.js
+++ public/vite.config.js
@@ -1,8 +1,20 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
   build: {
     outDir: 'dist',
     emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        converter: resolve(__dirname, 'converter.html'),
+        drawing: resolve(__dirname, 'drawing-mode.html'),
+        sheet: resolve(__dirname, 'sheet-mode.html'),
+        text: resolve(__dirname, 'text-mode.html'),
+        upload: resolve(__dirname, 'upload.html'),
+        404: resolve(__dirname, '404.html'),
+      }
+    }
   }
 });
